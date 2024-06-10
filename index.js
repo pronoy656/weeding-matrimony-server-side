2c0667b89a6bcccf7de2a198525e944b4867044d
pronoy656 <pronoypaul56@gmail.com>
2024-06-11 01:53:10 +0600

    vercel deployment

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
    
     // all database collection
     const bioDataCollection = client.db('Matrimonial').collection('biodata');
@@ -352,8 +352,8 @@ app.get('/user/approvalReq/:email', verifyToken, async(req,res) =>{
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
